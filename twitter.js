e8f536edfe061a6fb4c54299403ca7d64c713f56
yash <yashcr07@live.com>
2014-07-28 14:29:55 +0530

    add tweet

--- twitter.js
+++ twitter.js
@@ -1,6 +1,25 @@
-var express = require('express')
-var app = express.createServer()
+var express = require('express');
+var app = express();
+var bodyParser= require('body-parser')
+app.listen(8000);
+
 app.get('/', function(req, res) {
 res.send('Welcome to Node Twitter')
 })
-app.listen(8000);
+
+var tweets=[];
+app.post('/send',express.bodyParser(),function(req,res){
+	if(req.body&&req.body.tweet){
+		tweets.push(req.body.tweet)
+		res.send({status:"ok",messege:"Tweet received"})
+	}
+	else{
+		//no tweet
+		res.send({status:"nok",messege:"No tweet received"})
+	}
+})
+
+app.get('/tweets',function(req,res){
+	res.send(tweets)
+})
+
