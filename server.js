4740b48918b3adcdb69453a5eef3c641c2bd0a05
yash <yashcr07@live.com>
2014-08-24 19:27:28 +0530

    add feature (new line)

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.post('/send', express.bodyParser(), function(req, res)
 	if (req.body) 
 	{
 		tweets.push(req.body)
-		fs.writeFile("G:/Programming/NodeJS/ChatXpress/public/tweet.txt",JSON.stringify(req.body),function(er){
+		fs.appendFile("G:/Programming/NodeJS/ChatXpress/public/tweet.txt",JSON.stringify(req.body)+"\r\n",function(er){
 			if(er)
 				console.log(er)
 			else
